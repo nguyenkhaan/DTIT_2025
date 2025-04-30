87efa46d789010c7be756abea35859812dafd587
nguyenkhaan <nguyenkhaan2006@gmail.com>
2025-04-30 07:50:37 +0700

    image_question_v2

--- public/js/makePopup.js
+++ public/js/makePopup.js
@@ -7,6 +7,11 @@ function modalQuestionHTMLMaker(content)
     let ans = modalQuestion(content);  
     return ans; 
 }
+function modalQuestionAnsHTMLMaker(args) 
+{
+    let ans = questionChoiceList(args); 
+    return ans; 
+}
 export default function makePopup(level , content , args)
 {
     //Tiêu đề câu hỏi: Câu hỏi khó, dễ, trung bình,... 
@@ -16,6 +21,10 @@ export default function makePopup(level , content , args)
     //Xử lí nội dung câu hỏi 
     const modalQues = document.querySelector('.modal__question__box'); 
     modalQues.innerHTML = modalQuestionHTMLMaker(content)
+    //Xử lí nội dung câu trả lời 
+    const modalQuesAns = document.querySelector('.question__choice__wrapper'); 
+    modalQuesAns.innerHTML = modalQuestionAnsHTMLMaker(args); 
+    //Sự biến mất của pop up 
     const closeIcon = document.querySelector('.modal__header i') 
     closeIcon.onclick = () => {
         console.log('Hello world'); 
